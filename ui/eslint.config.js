2657f107ce5f4b0e2c7989be13c79732f4054fd0
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2026-09-22 16:30:55 +0900

    create angular project

---
+++ ui/eslint.config.js
@@ -0,0 +1,50 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const {defineConfig} = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = defineConfig([
+  {
+    files: ['**/*.ts'],
+    // Enable typed linting via the typescript-eslint Project Service.
+    // This is slower than untyped linting; see https://typescript-eslint.io/getting-started/typed-linting
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      angular.configs.tsRecommended,
+      prettierConfig,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'sk',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'sk',
+          style: 'kebab-case',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    rules: {},
+  },
+]);
