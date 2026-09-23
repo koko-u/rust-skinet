5a305a2691f38634060e23e78ac4d5c314651391
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2026-09-23 23:49:25 +0900

    change mat-table into normal table + tailwindcss

--- ui/src/app/features/products/products-table/products-table.ts
+++ ui/src/app/features/products/products-table/products-table.ts
@@ -1,35 +1,10 @@
 import { Component, input } from '@angular/core';
 import { Product } from '../product-model';
-import {
-  MatCell,
-  MatCellDef,
-  MatColumnDef,
-  MatHeaderCell,
-  MatHeaderCellDef,
-  MatHeaderRow,
-  MatHeaderRowDef,
-  MatRow,
-  MatRowDef,
-  MatTable,
-} from '@angular/material/table';
 import { MatButton } from '@angular/material/button';
 import { CurrencyPipe } from '@angular/common';
 
 @Component({
-  imports: [
-    MatTable,
-    MatColumnDef,
-    MatHeaderCell,
-    MatCell,
-    MatHeaderCellDef,
-    MatCellDef,
-    MatHeaderRow,
-    MatHeaderRowDef,
-    MatRow,
-    MatRowDef,
-    MatButton,
-    CurrencyPipe,
-  ],
+  imports: [MatButton, CurrencyPipe],
   selector: 'sk-products-table',
   styleUrl: './products-table.css',
   templateUrl: './products-table.html',
